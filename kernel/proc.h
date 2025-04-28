384b873c02f7b1041e1ad62d8551558fb9d34c9d
hyeinj <jhi2002@hanyang.ac.kr>
2025-04-28 19:43:20 +0900

    proc level 변수 추가

--- kernel/proc.h
+++ kernel/proc.h
@@ -104,4 +104,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+
+  // Project 1 관련 구현 부분
+  int level; // queue level which the process now belongs
 };
